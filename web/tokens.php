419da07d56ae169c2137731a02c619452be5dc6b
Rameal Nabeeh <rnabeeh@infosol.com>
2019-06-13 11:55:38 -0700

    Try to loop through icons

--- web/tokens.php
+++ web/tokens.php
@@ -1,6 +1,15 @@
 <?php
-echo "Hello Rameal";
-$check = 10;
-$check = $check + 10;
-echo $check;
+
+  if ($dir = opendir('./DnD Icons')) {
+    echo "Directory: $dir\n";
+    echo "Entries:\n";
+
+    /* This is the correct way to loop over the directory. */
+    while (false !== ($entry = readdir($dir))) {
+        echo "$entry\n";
+    }
+
+    closedir($dir);
+  }
+
 ?>
