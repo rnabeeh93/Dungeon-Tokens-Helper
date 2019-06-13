e2bca02ed005ed1d4bf81b3326508048d9ffe09e
Rameal Nabeeh <rnabeeh@infosol.com>
2019-06-13 13:21:24 -0700

    Try to get SVG Icon

--- web/tokens.php
+++ web/tokens.php
@@ -1,17 +1,36 @@
 <?php
 
-  if ($dir = opendir('./DnD Icons')) {
-    $tokens = array();
-
-    /* This is the correct way to loop over the directory. */
-    while (false !== ($entry = readdir($dir))) {
-        $split = str_replace(' ', '', explode(".", $entry)[0]);
-        if($split != ""){
-          array_push($tokens, $split);
-        }
-    }
-    closedir($dir);
-    echo(json_encode($tokens));
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
   }
 
+  $request = test_input($_GET["request"]);
+  switch ($request){
+    case("data"):
+      if ($dir = opendir('./DnD Icons')) {
+        $tokens = array();
+
+        /* This is the correct way to loop over the directory. */
+        while (false !== ($entry = readdir($dir))) {
+            $split = str_replace(' ', '', explode(".", $entry)[0]);
+            if($split != ""){
+              array_push($tokens, $split);
+            }
+        }
+        closedir($dir);
+        echo(json_encode($tokens));
+      }
+      break;
+    case("svg"):
+      $tname = "./DnD Icons/".test_input($_GET["token"]).".svg";
+      echo(file_get_contents($tname));
+      break;
+    default:
+      echo("ERROR: Request - " + $request);
+  }
+
+  die();
 ?>
