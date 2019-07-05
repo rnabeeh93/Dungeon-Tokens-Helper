c9f11f3097635493e6af8dac41e288f25a948b7e
Rameal Nabeeh <rnabeeh@infosol.com>
2019-07-05 16:25:36 -0700

    Replace dash with space

--- web/tokens.php
+++ web/tokens.php
@@ -15,7 +15,7 @@
 
         /* This is the correct way to loop over the directory. */
         while (false !== ($entry = readdir($dir))) {
-            $split = str_replace(' ', '', explode(".", $entry)[0]);
+            $split = str_replace('-', ' ', explode(".", $entry)[0]);
             if($split != ""){
               array_push($tokens, $split);
             }
